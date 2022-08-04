fd66e7b05d936d3240a10a769be8feeb8dcfeb45
nguyendinhkhoa19t1@gmail.com <nguyendinhkhoa19t1@gmail.com>
2022-08-04 22:02:06 +0700

    first commit

---
+++ src/main/kotlin/com/intern/CallVideoService/CallVideoServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.intern.CallVideoService
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CallVideoServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<CallVideoServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/intern/CallVideoService/CallVideoServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.intern.CallVideoService
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CallVideoServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
