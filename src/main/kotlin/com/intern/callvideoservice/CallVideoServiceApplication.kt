6a8b36339c62c228b9aa92df086a1eb50835a2c2
nguyendinhkhoa19t1@gmail.com <nguyendinhkhoa19t1@gmail.com>
2022-08-14 00:45:49 +0700

    implement swagger-ui

--- src/main/kotlin/com/intern/callvideoservice/CallVideoServiceApplication.kt
+++ src/main/kotlin/com/intern/callvideoservice/CallVideoServiceApplication.kt
@@ -1,9 +1,11 @@
 package com.intern.callvideoservice
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
+@OpenAPIDefinition
 @SpringBootApplication
 @EnableJpaAuditing
 class CallVideoServiceApplication
