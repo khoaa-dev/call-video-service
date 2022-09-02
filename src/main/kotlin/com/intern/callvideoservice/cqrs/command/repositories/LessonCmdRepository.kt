2c38e0d70e09ea000d74f6b2c9c422b80b7809ea
nguyendinhkhoa19t1@gmail.com <nguyendinhkhoa19t1@gmail.com>
2022-09-02 14:31:30 +0700

    remove lÃesson sáºervice

--- src/main/kotlin/com/intern/callvideoservice/cqrs/command/repositories/LessonCmdRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.intern.callvideoservice.cqrs.command.repositories
-
-import com.intern.callvideoservice.cqrs.command.domain.LessonCmd
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface LessonCmdRepository : JpaRepository<LessonCmd, Long>
